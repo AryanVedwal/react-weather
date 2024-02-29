3b5ca456869c0242f0be0bfd9bdd61d65332b2f9
AryanVedwal <aryanvedwal01@gmail.com>
2024-02-29 16:07:14 +0530

    initial commit

---
+++ src/info.js
@@ -0,0 +1,28 @@
+const temp = [
+  {
+    coord: { lon: 77.55, lat: 29.9667 },
+    weather: [
+      { id: 800, main: "Clear", description: "clear sky", icon: "01d" },
+    ],
+    base: "stations",
+    main: {
+      temp: 25.52,
+      feels_like: 24.68,
+      temp_min: 25.52,
+      temp_max: 25.52,
+      pressure: 1015,
+      humidity: 21,
+      sea_level: 1015,
+      grnd_level: 983,
+    },
+    visibility: 10000,
+    wind: { speed: 4.09, deg: 296, gust: 4.57 },
+    clouds: { all: 0 },
+    dt: 1709187259,
+    sys: { country: "IN", sunrise: 1709169415, sunset: 1709210884 },
+    timezone: 19800,
+    id: 1257806,
+    name: "Sahāranpur",
+    cod: 200,
+  },
+];
